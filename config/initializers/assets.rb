712eb95b7087b69721a6b3712e0d993507e0a93a
Daniel Monroy <ldanielmonroy@gmail.com>
2016-03-21 09:07:49 -0600

    Solucionado dropdowns

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( css/bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( css/style.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
