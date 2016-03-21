e92cf95009981f74bcff334744c46fd7d87a58cd
ricardorodab <ricardorodab19@gmail.com>
2016-03-21 03:43:12 -0600

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+99.times do |n|
+  nombre = Faker::Name.name
+  apellido = Faker::Name.last_name
+  blood = "A+"
+  correo = "persona-#{n+1}@ejemplo.com"
+  password = "password"
+ m = MUser.create!(m_user_name: nombre,
+                m_user_surname: apellido,
+                blood_type_user: blood,
+                email: correo,
+                password: password)
+  titulo = Faker::Lorem.sentence(3)
+  blood2 = "A+"
+  location = "Tabasco"
+  phone = "5553259#{n}03"
+  text = Faker::Lorem.sentence
+  m_user = n+1
+  m.posts.create!(title: titulo,
+                  blood_type_post: blood2,
+                  location_post: location,
+                  phone_post: phone,
+                  description: text)
+end
